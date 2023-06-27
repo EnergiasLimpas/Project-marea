8251811fba5c3d7709e3c26dd787e724969b45d5
Helder Thadeu <68792636+EnergiasLimpas@users.noreply.github.com>
2023-06-27 18:40:51 -0300

    Update Esp32_projeto_liec_v4.ino

--- Esp32_projeto_liec_v2.ino
+++ Esp32_projeto_liec_v2.ino
@@ -4,10 +4,11 @@
 #include "img_converters.h"
 #include "Arduino.h"
 #include "fb_gfx.h"
-#include "soc/soc.h" 
-#include "soc/rtc_cntl_reg.h"  
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
 #include "esp_http_server.h"
 #include <BluetoothSerial.h>
+#include <analogWrite.h>
 
 
 //  ============================================================
@@ -26,15 +27,15 @@ int valor_giro;
 //  ============================================================
 // const char* ssid     = "Vivo-Internet-ED42";
 // const char* password = "32425121";
-const char* ssid     = "2G_CLARO_IGOR";
+const char* ssid = "2G_CLARO_IGOR";
 const char* password = "12121998";
 
 
 //  ============================================================
 //  Configurações do Sensor Ultrassônico
 //  ============================================================
-const int pin_trigger = 12; // 5
-const int pin_echo = 14; // 18
+const int pin_trigger = 33;  // 5
+const int pin_echo = 32;     // 18
 #define SOUND_SPEED 0.034
 long duration;
 float distanciaCm;
@@ -43,64 +44,75 @@ int distanciaminima = 10;
 //  ============================================================
 //  Configurações do Buzzer
 //  ============================================================
-const int pin_buzzer = 27; // 32
+const int pin_buzzer = 34;  // 32
 
 //  ============================================================
 //  Configurações dos Motores
 //  ============================================================
-const int pin_motor_4 = 25; // 14
-const int pin_motor_3 = 33; // 27
-const int pin_motor_2 = 32; // 26
-const int pin_motor_1 = 35; // 25
-const int pin_motor_velocidade_direita = 34; // 12
-const int pin_motor_velocidade_esquerda = 33; // 33
+const int pin_motor_4 = 14;                    // 14
+const int pin_motor_3 = 27;                    // 27
+const int pin_motor_2 = 26;                    // 26
+const int pin_motor_1 = 25;                    // 25
+const int pin_motor_velocidade_direita = 12;   // 12
+const int pin_motor_velocidade_esquerda = 13;  // 33
 
 //  Função de movimento
-void movement(char move, int giro)
-{
+void movement(char move, int giro) {
   //  Curva
   int vel_esquerda;
   int vel_direita;
 
-  if(giro < 0){
-    giro = giro * (-1);
-    vel_esquerda = 80 + (giro * 15);
-    vel_direita = 230;
-  }
-  else if(giro > 0){
-    vel_esquerda = 230;
-    vel_direita = 80 + (giro * 15);
-  }
-  else{
-    vel_esquerda = 230;
-    vel_direita = 230;
+  if (giro < 10) {
+    vel_esquerda = giro * 51;
+    vel_esquerda += 306;
+    vel_direita = 255;
+  } else if (giro > 10  && giro<16) {
+    vel_esquerda = 255;
+    vel_direita = giro * 51;
+    vel_direita *= -1;
+    vel_direita += 816;
+  } else if (giro >= 16) {
+    vel_esquerda = 255;
+    vel_direita = 0;
+  } else {
+    vel_esquerda = 255;
+    vel_direita = 255;
   }
 
+  Serial.print("Velocidade na esquerda: ");
+  Serial.println(vel_esquerda);
+  Serial.print("Velocidade na direita: ");
+  Serial.println(vel_direita);
+  Serial.print("Valor de giro: ");
+  Serial.println(giro);
+  Serial.print("Letter move: ");
+  Serial.println(move);
   //  Frente
-  if(move == 'w'){
+  if (move == 'w') {
     digitalWrite(pin_motor_1, HIGH);
     digitalWrite(pin_motor_2, LOW);
     digitalWrite(pin_motor_3, LOW);
     digitalWrite(pin_motor_4, HIGH);
-    analogWrite(pin_motor_velocidade_direita,vel_direita);
-    analogWrite(pin_motor_velocidade_esquerda,vel_esquerda);
+    analogWrite(pin_motor_velocidade_direita, vel_direita);
+    analogWrite(pin_motor_velocidade_esquerda, vel_esquerda);
   }
   //  Direita
-  else if(move == 'd'){
-    digitalWrite(pin_motor_1, LOW);
+  else if (move == 'd') {
+    digitalWrite(pin_motor_1, HIGH);
     digitalWrite(pin_motor_2, HIGH);
-    digitalWrite(pin_motor_3, LOW);
+    digitalWrite(pin_motor_3, HIGH);
     digitalWrite(pin_motor_4, HIGH);
   }
   //  Esquerda
-  else if(move == 'a'){
-    digitalWrite(pin_motor_1, HIGH);
+  else if (move == 'a') {
+    digitalWrite(pin_motor_1, LOW);
     digitalWrite(pin_motor_2, LOW);
     digitalWrite(pin_motor_3, HIGH);
     digitalWrite(pin_motor_4, LOW);
+
   }
   //  Ré
-  else if(move == 's'){
+  else if (move == 's') {
     digitalWrite(pin_motor_1, LOW);
     digitalWrite(pin_motor_2, HIGH);
     digitalWrite(pin_motor_3, HIGH);
@@ -108,16 +120,36 @@ void movement(char move, int giro)
     // analogWrite(pin_motor_velocidade_direita,0);
     // analogWrite(pin_motor_velocidade_esquerda,80);
     // analogWrite(pin_motor_velocidade_esquerda,255);
-    analogWrite(pin_motor_velocidade_direita,vel_direita);
-    analogWrite(pin_motor_velocidade_esquerda,vel_esquerda);
+    analogWrite(pin_motor_velocidade_direita, vel_direita);
+    analogWrite(pin_motor_velocidade_esquerda, vel_esquerda);
   }
   //  Parada
-  else if(move == 'p'){
+  else if (move == 'p') {
     digitalWrite(pin_motor_1, LOW);
     digitalWrite(pin_motor_2, LOW);
     digitalWrite(pin_motor_3, LOW);
     digitalWrite(pin_motor_4, LOW);
+    analogWrite(pin_motor_velocidade_direita, 0);
+    analogWrite(pin_motor_velocidade_esquerda, 0);
   }
+}
+
+// Função sensor ultrassonico
+float distance_sensor() {
+  digitalWrite(pin_trigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pin_trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pin_trigger, LOW);
+  duration = pulseIn(pin_echo, HIGH);
+  float distancia = duration * SOUND_SPEED / 2;
+  return distancia;
+}
+// Função do buzzer
+void reverse_sound(float distancia) {
+  int valor_som = distancia * -36.428571;
+  valor_som += 255;
+  analogWrite(pin_buzzer, valor_som);
 }
 
 //  ============================================================
@@ -151,40 +183,80 @@ void setup() {
 //  Loop
 //  ============================================================
 void loop() {
-  if (SerialBT.available() > 0){
-  // if(begin == 1){
-    char letter_move = 's';
-    valor_recebido =(char)SerialBT.read();
-    valor_giro =(int)SerialBT.read();
+  if (SerialBT.available() > 0) {
+    // if(begin == 1){
+    distance_sensor();
+    reverse_sound(distance_sensor());
+    char letter_move = 'p';
+    valor_recebido = (char)SerialBT.read();
+    valor_giro = (int)SerialBT.read();
+
+
+
 
     //  Le as informações em cm do sensor ultrassônico
-    digitalWrite(pin_trigger, LOW);
-    delayMicroseconds(2);
-    digitalWrite(pin_trigger, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(pin_trigger, LOW);
-    duration = pulseIn(pin_echo, HIGH);
-    distanciaCm = duration * SOUND_SPEED/2;
 
-    //  Impressão de resultados 
-    Serial.print("Distancia em cm: ");
-    Serial.println(distanciaCm);
-    // Serial.println(valor_recebido);
+    //  Impressão de resultados
 
     //  Movimento Botões
     // movement(letter_move, 8);
-    if(valor_recebido == 'A'){
+    if (valor_recebido == 'A') {
       letter_move = 'w';
+      Serial.print("Valor recebido: ");
+      Serial.println(valor_recebido);
+      Serial.print("Valor de giro: ");
+      Serial.println(valor_giro);
+      while (valor_recebido != 'Z') {
+        valor_recebido = (char)SerialBT.read();
+        valor_giro = (int)SerialBT.read();
+        if (valor_recebido == 'Z') {
+          letter_move = 'p';
+          movement(letter_move, valor_giro);
+        } else if (valor_giro > 0 && valor_giro < 20) {
+          valor_recebido = (char)SerialBT.read();
+          if (valor_recebido == 'Z') {
+            letter_move = 'p';
+            movement(letter_move, valor_giro);
+          } else {
+            movement('w', valor_giro);
+          }
+        }
+      }
+    } else if (valor_recebido == 'R' && distance_sensor() > 10) {
+      letter_move = 's';
       movement(letter_move, valor_giro);
-    }
-    else if(valor_recebido == 'Z'){
+      while (valor_recebido != 'Z' && distance_sensor() > 10) {
+        Serial.print("Distancia: ");
+        Serial.println(distance_sensor());
+        reverse_sound(distance_sensor());
+        valor_giro = (int)SerialBT.read();
+        valor_recebido = (char)SerialBT.read();
+        if (distance_sensor() < 10 || valor_recebido == 'Z') {
+          letter_move = 'p';
+          movement(letter_move, valor_giro);
+        } else if (valor_giro > 0 && valor_giro < 20 && valor_recebido != 'Z') {
+          valor_recebido = (char)SerialBT.read();
+          if (distance_sensor() < 10 || valor_recebido == 'Z') {
+            letter_move = 'p';
+            movement(letter_move, valor_giro);
+          }
+          movement(letter_move, valor_giro);
+        }
+      }
+    } else if (valor_recebido == 'Z' || distance_sensor() < 10) {
+      reverse_sound(distance_sensor());
+      Serial.print("Valor recebido: ");
+      Serial.println(valor_recebido);
       letter_move = 'p';
       movement(letter_move, valor_giro);
     }
-    else if(valor_recebido == 'R'){
-      letter_move = 's';
-      movement(letter_move, valor_giro);
-    }
+    // else if(valor_recebido == 'D'){
+    //   letter_move = 'd';
+    //   movement(letter_move, valor_giro);
+    // }
+    // else if(valor_recebido == 'E'){
+    //   letter_move = 'a';
+    //   movement(letter_move, valor_giro);
+    // }
   }
 }
-
